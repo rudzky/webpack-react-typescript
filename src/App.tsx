0f7ee071068fdd3069eba9acdf9df5634b7011d9
Rudzky <lubieprogramowanie@gmail.com>
2021-05-24 12:58:02 +0200

    Add React-Refresh plugin for development use

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./style.css";
 import "./customStyles.scss";
 import Peter from "./peter.jpg";
 import FlutterLogo from "./flutter.svg";
+import { ClickCounter } from "./ClickCounter";
 
 export const App = () => {
   return (
@@ -12,6 +13,7 @@ export const App = () => {
       </h2>
       <img src={Peter} alt="Just Peter Griffin" width="300" height="200" />
       <img src={FlutterLogo} alt="Just flutter logo" width="200" height="200" />
+      <ClickCounter />
     </>
   );
 };
