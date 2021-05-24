cf9d716430edc783a2ea5f2f433dad25596312da
Rudzky <lubieprogramowanie@gmail.com>
2021-05-24 11:53:32 +0200

    Add CSS and SASS loaders

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import "./style.css";
+import "./customStyles.scss";
+
 export const App = () => {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <h2>What a wonderful day!!</h2>
+    </>
+  );
 };
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -21,6 +21,14 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
   },
   resolve: {
